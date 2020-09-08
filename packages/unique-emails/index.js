81badaa26903341c861a4d2d3c22eda748440c26
Prescott Traversi <ptraversi@gmail.com>
2020-09-08 15:03:45 -0700

    Number of Unique Emails

---
+++ packages/unique-emails/index.js
@@ -0,0 +1,29 @@
+/**
+ *
+ * Uniqe emails: https://leetcode.com/problems/unique-email-addresses/
+ *
+ * Time: O(x) where x is the total char count of array input
+ * Space: O(n)
+ */
+const numUniqueEmails = (emails) => {
+    const set = new Set();
+    for (const email of emails) {
+        let result = '';
+        let ignore = false;
+        for (let i = 0; i < email.length; i++) {
+            if (email[i] === '@') {
+                result += email.slice(i);
+                set.add(result);
+                break;
+            } else if (email[i] === '+') {
+                ignore = true;
+            } else if (!ignore && email[i] !== '.') {
+                result += email[i];
+            }
+        }
+    }
+    return set.size;
+};
+
+export default numUniqueEmails;
+
---
+++ packages/unique-emails/index.test.js
@@ -0,0 +1,9 @@
+import numUniqueEmails from './index';
+
+test('Test case 1', () => {
+  expect(numUniqueEmails(["test.email+alex@abc.com","test.e.mail+bob.cathy@abc.com","testemail+david@a.bc.com"])).toBe(2);
+});
+test('Test case 2', () => {
+  expect(numUniqueEmails(["testemail@abc.com", "test.email@abc.org", "testemail+eight@abc.gov"])).toBe(3);
+});
+
