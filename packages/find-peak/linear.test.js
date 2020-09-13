c00fe69c566e7f17e2a83ae0e79ddca5b397ed87
prescotttrav <46552614+prescotttrav@users.noreply.github.com>
2020-09-13 14:55:28 -0700

    Update find peak tests

--- packages/find-peak/linear.test.js
+++ packages/find-peak/linear.test.js
@@ -7,7 +7,7 @@ test('Test case 2', () => {
   expect(findPeak([1,2,3,4,5,6,7,8])).toBe(7);
 });
 test('Test case 3', () => {
-  expect(findPeak([8,7,6,5,4,3,2,1])).toBe(7);
+  expect(findPeak([8,7,6,5,4,3,2,1])).toBe(0);
 });
 test('Test case 4', () => {
   expect(findPeak([1,1,1,1,1,1,1])).toBe(-1);
