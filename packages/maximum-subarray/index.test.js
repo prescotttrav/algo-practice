37c33f2d0ab8c7e8b87f39ab13a67502725fc287
Prescott Traversi <ptraversi@gmail.com>
2020-09-23 17:13:09 -0700

    Include greedy approach

--- packages/maximum-subarray/index.test.js
+++ packages/maximum-subarray/index.test.js
@@ -1,4 +1,5 @@
 import divideAndConquer from './divide-and-conquer';
+import greedy from './greedy';
 
 describe('Test case 1', () => {
   const input = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
@@ -6,11 +7,17 @@ describe('Test case 1', () => {
   it('divide and conquer', () => {
     expect(divideAndConquer(input)).toBe(res);
   });
+  it('greedy', () => {
+    expect(greedy(input)).toBe(res);
+  });
 });
 describe('Test case 2', () => {
   const input = [-1, 10, -8, 2, 3, -16, 6, 2, -5, -12, 9, 12, -22];
   const res = 21;
   it('divide and conquer', () => {
     expect(divideAndConquer(input)).toBe(res);
+  });
+  it('greedy', () => {
+    expect(greedy(input)).toBe(res);
   });
 });
